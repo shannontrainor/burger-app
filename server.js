120c1f97aae94b160a8e85710d200a23cc652904
Shannon Trainor <sltrainor@outlook.com>
2020-06-25 12:44:23 -0400

    edited pseudocoding

--- server.js
+++ server.js
@@ -1,61 +1,63 @@
-// check cats app week 13
+// Use CATSAPP Activity 17
     //handle bars files will help
 
 
+TODO
+//1. create schema first
+//2. Create seeds
+//3. Create index.handlebars & main.handlebars -- reference catsapp
+        //handlebars: each = for loop, unless = if conditonal
 
-//create schema first
-//seeds next
+//4. NEED 4 MAIN API ROUTES
+    //a. devoured
+    //b. not devoured
+    //c. create new burger
+    //d. turning not devoured (false) to devoured (true)
 
-//create handlebars
-//each = for loop
-//unless = if conditional
+//5. Set up server.js
+    //require express, handlebars
+    //connect to controller.js
 
-//4 MAIN API ROUTES
-//devoured
-//not devoured
-//create newburger
-//turning from false to true (not devoured to devoured)
+//6. Set up controller.js
 
-//next set up server.js - require express, handlebars, connect controller
-    // then conotroller file = api routs, get routes, post, put
-        // res.render
+//7. Need API routes in burger.js - get/post/put
+        //then res.render
+        // burger.all = burger is object, all is key (function)
 
-            // burger.all = burger is object, all is key (function)
+//8. Create JS folder w/ script.js
+    //a. create onclick for devour burger on devour button
+    //b. update DB from false to true
+    //c. create Ajax call to update PUT method
+        //update ID of burger & send to route
 
-//js in public - for onclick for devour burger on devour button
-    //update database from dalse to true 
-    //ajax call to update put method, update ID Of burger, send to route
+//9. Create connection.js
 
-//then connection.js
-//contoller.js
-    //require express, router (express.Router();), require burger.js from models
-    //router.get("/")
-        //res.redirect("/burgers")
-    //in controller, var burger = (route to burger.js)
+//10. Creater controller.js
+    //a. require express, router (express.Router();), require burger.js from models
+    //b. start router.get("/")
+        //res.redirect("/burgers") -- create redirect when user clicks, will redirect to burgers page
+    //c. in controller.js, var burger = (route to burger.js)
 
-
-//orm.js 3 keys - all, create, update
-    //var orm = {all,create,update}
-    //sends queery before commncating with batase
-    //imported to burger.js in models folder
-    //orm.all requires 2 arguments
-    //need query, connection.query, error function
+//11. Create orm.js
+    //import INTO burger.js
+    //a. 3 keys -- all, create, update
+        // var orm = {all, create, update} object
+            //orm.all requires 2 arguments
+    //b. send query before communicating with DB
+    //c. need query, connection.query, error function
         //if no error, call function cb(results)
 
+//12. Create burger.js
+    //a. require orm.js file
+    //b. var burger defined - addEventListener, create, update
+    //c. burger.all
 
-//in burger.js
-    // var burger defined - addEventListenerll, create, update
-    //require orm.js file
-    //burger.all
+//13. address main.handlebars
+    //a. will contain all script tags
+    // will render js
 
-
-//main.handlbars will render js
-    //have all script tags
-
-
-//pseudocode
-
-//heroku use jawsdb or mlabs, mongoatlas
+//**EXTRA* */
+//heroku -- use jawsdb, mlabs, mongoatlas
     //make sure to add to package.json - script tag w/ start "node server.js"
 
 

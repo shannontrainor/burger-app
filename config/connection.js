e6e1436d5ead5ab2b4ea844912819ad8ad991f9b
Shannon Trainor <sltrainor@outlook.com>
2020-06-24 21:45:42 -0400

    added jawsdb to connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 //require MYSQL
 const mysql = require("mysql");
 
-//create connection with .env
-const connection = mysql.createConnection({
+//connection with JAWSDB
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //create connection with .env
+  const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
-});
+  });
+};
 
 connection.connect((err) => {
     if (err) {

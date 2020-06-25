b90803378069e489bb6d007e953aefa18b247a99
Shannon Trainor <sltrainor@outlook.com>
2020-06-24 22:20:27 -0400

    fixed connection error

--- config/connection.js
+++ config/connection.js
@@ -13,15 +13,19 @@ if (process.env.JAWSDB_URL) {
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
   });
-};
+}
 
 connection.connect((err) => {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
+  if (err) throw error;
+  console.log("DB connected");
 });
+
+//     if (err) {
+//       console.error("error connecting: " + err.stack);
+//       return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 
 //export
 module.exports = connection;

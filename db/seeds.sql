7c89ebbe4a2f2c69c98b1499b3bff3d839980468
Shannon Trainor <sltrainor@outlook.com>
2020-06-24 21:05:27 -0400

    insert data to db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger", FALSE);

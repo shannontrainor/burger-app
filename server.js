9ac4e42f766cb2f198a2957950c5145d3cf948c9
Shannon Trainor <sltrainor@outlook.com>
2020-06-24 20:49:38 -0400

    added pseudocoding

---
+++ server.js
@@ -0,0 +1,67 @@
+// check cats app week 13
+    //handle bars files will help
+
+
+
+//create schema first
+//seeds next
+
+//create handlebars
+//each = for loop
+//unless = if conditional
+
+//4 MAIN API ROUTES
+//devoured
+//not devoured
+//create newburger
+//turning from false to true (not devoured to devoured)
+
+//next set up server.js - require express, handlebars, connect controller
+    // then conotroller file = api routs, get routes, post, put
+        // res.render
+
+            // burger.all = burger is object, all is key (function)
+
+//js in public - for onclick for devour burger on devour button
+    //update database from dalse to true 
+    //ajax call to update put method, update ID Of burger, send to route
+
+//then connection.js
+//contoller.js
+    //require express, router (express.Router();), require burger.js from models
+    //router.get("/")
+        //res.redirect("/burgers")
+    //in controller, var burger = (route to burger.js)
+
+
+//orm.js 3 keys - all, create, update
+    //var orm = {all,create,uodate}
+    //sends queery before commncating with batase
+    //imported to burger.js in models folder
+    //orm.all requires 2 arguments
+    //need query, connection.query, error function
+        //if no error, call function cb(results)
+
+
+//in burger.js
+    // var burger defined - addEventListenerll, create, update
+    //require orm.js file
+    //burger.all
+
+
+//main.handlbars will render js
+    //have all script tags
+
+
+//pseudocode
+
+//heroku use jawsdb or mlabs, mongoatlas
+    //make sure to add to package.json - script tag w/ start "node server.js"
+
+
+
+
+
+
+
+    

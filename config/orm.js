a9f94326890e7b052d4a731188df20e3fe2a0498
Shannon Trainor <sltrainor@outlook.com>
2020-06-28 18:11:48 -0400

    added mysql helper syntax

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,24 @@
 //require connection
 const connection = require("./connection.js")
 
+//create array of ? and turn to string, help write to query
+function printQuestionMarks(num) {
+    let arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+};
+
+//turns obj key to mysqlsyntax
+function objToSql(ob) {
+    let arr = [];
+    for (var key in ob) {
+        arr.push(key + "=" + ob[key]);
+    }
+    return arr.toString();
+}
+
 //object to access all, create, and update burgers
 const orm = {
     all: (tableInput, cb) => {
